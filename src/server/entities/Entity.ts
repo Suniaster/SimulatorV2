8c4d32117bda52d435d8bd68b84df17ef9af7a6a
Suniaster <thibousbr@gmail.com>
2020-05-04 10:39:47 -0300

    Adds callbacks to movement

--- src/server/entities/Entity.ts
+++ src/server/entities/Entity.ts
@@ -17,8 +17,26 @@ export type Vector = {
 export default abstract class Entity extends Polygon{
     protected vel: Point;
     protected accel: Point;
+    protected movementCallbacks:{
+        beforeMove:()=>void, 
+        afterMove:()=>void,
+        beforeExectute:()=>void,
+        afterExecute:()=>void
+    }
     public readonly type: string;
-
+    
+    /**
+     * ### Movement
+     * 
+     * @field movementCallbacks.beforeMove its called only when object moves
+     * @field movementCallbacks.afterMove  its called after object moves
+     * @field movementCallbacks.beforeExectute its called always before function starts
+     * @field movementCallbacks.afterExecute  its called always right before function returns
+     * 
+     * Order of execution:
+     * 
+     * beforeExecute -> beforeMove -> position changes -> afterMove -> afterExecute
+     */
     constructor(position: Point, public size?: {width:number, height:number}, points?:number[][], public readonly id = Entity.makeid(6)){
         super(position.x, position.y, points)
         this.vel = {
@@ -28,6 +46,12 @@ export default abstract class Entity extends Polygon{
         this.accel = {
             x: 0,
             y: 0
+        }
+        this.movementCallbacks = {
+            beforeMove: ()=>{}, 
+            afterMove: ()=>{},
+            beforeExectute: ()=>{},
+            afterExecute: ()=>{}
         }
     }
 
@@ -59,16 +83,21 @@ export default abstract class Entity extends Polygon{
      *  return a boolean indicating if entity has moved
      */
     public move(): boolean{
+        this.movementCallbacks.beforeExectute();
         this.updateVel();
 
         let dt = 1/config.server.fps
         let dt_2 = (dt*dt)
         if(this.vel.x !== 0 || this.vel.y !== 0){
+            this.movementCallbacks.beforeMove();
             this.x += (this.vel.x * dt) + (dt_2*this.accel.x * 0.5)
             this.y += (this.vel.y * dt)+ (dt_2*this.accel.y * 0.5) 
+            this.movementCallbacks.afterMove();
+            this.movementCallbacks.afterExecute();
             return true;
         }
         else{
+            this.movementCallbacks.afterExecute();
             return false;
         }        
     }
