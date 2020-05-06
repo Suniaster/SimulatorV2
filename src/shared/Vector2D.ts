7228a6fa38c0710b0cdbcbc463904c67e855995b
Suniaster <thibousbr@gmail.com>
2020-05-06 12:18:12 -0300

    Creates class Vector2D

---
+++ src/shared/Vector2D.ts
@@ -0,0 +1,74 @@
+
+
+export default class Vector2D{
+    constructor(public x, public y){}
+
+    static add(v1: Vector2D, v2: Vector2D){
+        return new Vector2D(v1.x+v2.x, v1.y+v2.y)
+    }
+
+    static sub(v1: Vector2D, v2: Vector2D){
+        return new Vector2D(v1.x-v2.x, v1.y-v2.y)
+    }
+
+    static random(mag = 1){
+        let newVector = new Vector2D(mag, 0);
+        newVector.rotate(Math.random()*Math.PI*2)
+        return newVector;
+    }
+
+    public add(vector: Vector2D){
+        this.x += vector.x;
+        this.y += vector.y;        
+        return this
+    }
+
+    public sub(vector: Vector2D){
+        this.x -= vector.x;
+        this.y -= vector.y;        
+        return this
+    }
+
+    public mag(){
+        return Math.sqrt(
+            (this.x*this.x) + (this.y*this.y)
+        )
+    }
+
+    public setMag(mag:number){
+        let angle = this.heading();
+
+        this.x = Math.cos(angle) * mag;
+        this.y = Math.sin(angle) * mag;
+    }
+
+    public heading(){
+        let hip = this.mag()
+        if(hip === 0 )return 0
+        return Math.acos(
+            this.x/hip
+        )
+    }
+
+    public dist(vector: Vector2D){
+        return Math.sqrt(
+            ((vector.x - this.x)*(vector.x - this.x)) + ((vector.y - this.y)*(vector.y - this.y)) 
+        )
+    }
+
+    public limit(max:number){
+        if( this.mag() > max ){
+            this.setMag(max);
+        }
+    }
+
+    public rotate(angle:number){
+        if(angle === 0) return;
+        let newAngle = (angle + this.heading()) % (2*Math.PI)
+        let mag = this.mag();
+
+        this.x = Math.cos(newAngle) * mag;
+        this.y = Math.sin(newAngle) * mag; 
+    }
+
+}
